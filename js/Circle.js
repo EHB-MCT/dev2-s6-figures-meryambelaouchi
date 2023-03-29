898939e23c435b8bae4ed68447cdc17391d3995f
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2023-03-29 16:05:57 +0200

    a step further

--- js/Circle.js
+++ js/Circle.js
@@ -2,5 +2,20 @@
 
 import Figure from './Figure.js';
 
-export class 
+export default class Circle extends Figure {
+    constructor(name, radius) {
+        super(name);
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
 
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    get circumference() {
+        return Math.PI * 2 * this.radius;
+    }
+}
--- js/Rectangle.js
+++ js/Rectangle.js
@@ -2,7 +2,7 @@
 
 import Figure from './Figure.js';
 
-export default class Rectangle {
+export default class Rectangle extends Figure {
     constructor(name, width, heigth) {
         super(name);
         this.width = width;
@@ -13,5 +13,26 @@ export default class Rectangle {
     }
     get circumference() {
         return (this.width + this.height) * 2;
+    }
+    get HTMLString() {
+        return `<div class="result">
+        <div class="figure circle" style="width: DIAMETERpx; height: DIAMETERpx;"></div>
+        <div class="infoBox">
+          <h3>Circle</h3>
+          <dl>
+            <dt>Radius</dt>
+            <dd>RADIUS px</dd>
+      
+            <dt>Diameter</dt>
+            <dd>DIAMETER px</dd>
+      
+            <dt>Area</dt>
+            <dd>AREA px</dd>
+      
+            <dt>Circumference</dt>
+            <dd>CIRCUMFERENCE px</dd>
+          </dl>
+        </div>
+      </div>`;
     }
 }
